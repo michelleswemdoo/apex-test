a00bb3c3f8c3f97db245a342a266778d57c7622f
michelleswem <swemmichelle990@gmail.com>
2024-05-08 18:30:44 -1200

    Add page layout

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-white">
+    <div class="max-w-screen-lg m-auto py-5 items-center justify-between flex">
+      <h1 class="text-3xl">Table Heading</h1>
+
+      <div class="flex items-center">
+        <img
+          src="https://s3-alpha-sig.figma.com/img/b840/0f45/f3982a3f38174ada0e7b09506ea13edc?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nDn4ibEOQrNIugcDUFkX1xJCjD0pHOR8RuL0qRHHfI3FAef3xjWRYu0MCJ6R1dwaYIQ3TjviPjW9aqp1AXnAIc1nBbuFLwjIlRWPePRjhm0rbeq2~0m7waXOjFNgrdQfM~-Q2tGTjc9huJ3NRT5ApuzzTrODPUrZJCF2HGWoPAAx1Yn6cnG8uv8jS7dnGhjx1yhc~Bxd0Gu07KqLB3XA1UZ8awhOiyreDvUF~jtXGUuWxLR~R0HPBF0dca2SXNsOlVldnbEcABCrIgKsR8iEqwQeRNrWE2KfB-267vohXTw5fgRmkXJedWy5nVpG7r7CJf3axNx7R0S3s33CNoFtBQ__"
+          alt="profile"
+          class="h-12 w-12 mr-3 rounded-full object-cover bg-yellow-600"
+        />
+        <div>
+          <p class="mb-1 font-semibold">Tynisha Obey</p>
+          <p class="text-sm">Makostore</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <AppHeader />
+
+  <div class="max-w-screen-lg m-auto my-8 rounded-lg">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import AppHeader from '@/components/AppHeader.vue';
+</script>
