f5a92a5350fad3ae0aeec0f541ff59966d9c8d36
michelleswem <swemmichelle990@gmail.com>
2024-05-09 13:56:06 -1200

    Add AppBadgeChip component

---
+++ src/components/ui/AppBadgeChip.vue
@@ -0,0 +1,12 @@
+<template>
+  <p class="w-fit px-3 py-1.5 rounded-lg capitalize mb-2 flex items-center">
+    <span class="mr-2">
+      <CircleChipIcon />
+    </span>
+    <slot />
+  </p>
+</template>
+
+<script setup lang="ts">
+import CircleChipIcon from '@/assets/icons/CircleChipIcon.vue';
+</script>
